737140174888fb70c67ad29ff61f6356ba5c17e7
ashish rajput <ashish.0017.rajput@gmail.com>
2023-09-18 17:09:05 +0530

    Basic Structure of House App

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Main(){
+    return (
+        <>
+        <section className="house-list">
+            <div className="house-list--intro">
+                <h1 className="heading__secodnary">Get Home Of Your Dream.</h1>
+            </div>
+            <div className="house-list--item">
+                <img src="" alt="" />
+                <h1>Hello There</h1>
+                <p>Here is the Best Villa Available to buy</p>
+            </div>
+        </section>
+        </>
+    );
+}
+export default Main;
